72ac80d825e947fd90fa05c5af422efc0a9ce658
Vijayakumar Moorthy <vijayakumar.moorthy@gmail.com>
2018-06-09 21:06:48 +0530

    upload added

--- user/upload.php
+++ user/upload.php
@@ -1,86 +1,91 @@
-<?php
+ <?php
 include '../lib_security.php';
 
+/**
 echo "<pre>test";
 print_r($_REQUEST);
 print_r($_FILES);
-$uid = $_REQUEST['uid'];
+/**/
 
-$filename = $_FILES['idproof']['name'];
+uploadFile('idproof');
+echo "<br/>";
+uploadFile('addressproof');
 
-$target_dir = "/HLS/data/user/".$uid."/";
-if (!file_exists($target_dir)) {
-    mkdir($target_dir, 0777, true);
-}
-$target_file = $target_dir . basename($_FILES["idproof"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-$target_file = $target_dir . "idproof.".$imageFileType;
-$target_file_jpg = $target_dir . "idproof.jpg";
-// Check if image file is a actual image or fake image
-if(true) {
-    $check = getimagesize($_FILES["idproof"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
-// Check file size
-if ($_FILES["idproof"]["size"] > 50*1024*1024) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["idproof"]["tmp_name"], $target_file_jpg)) {
-        echo "The file ". basename( $_FILES["idproof"]["name"]). " has been uploaded.";
+$uid = isset($_REQUEST['uid']) ? $_REQUEST['uid'] : 1;
+$idProof = "/data/user/{$uid}/idproof.jpg";
+$addressProof = "/data/user/{$uid}/addressproof.jpg";
 
-	echo "<BR/>FILE TYPE: ".$imageFileType;
-
-
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
-?>
-<h1>Photo uploaded</h1>
-
-
-
-<?php 
-function convertImage($originalImage, $outputImage, $quality=100)
+function uploadFile($id)
 {
-    // jpg, png, gif or bmp?
-    $exploded = explode('.',$originalImage);
-    $ext = $exploded[count($exploded) - 1]; 
+	$uid = $_REQUEST['uid'];
+	$target_dir = "/HLS/data/user/".$uid."/";
+	if (!file_exists($target_dir)) {
+	    mkdir($target_dir, 0777, true);
+	}
 
-    if (preg_match('/jpg|jpeg/i',$ext))
-        $imageTmp=imagecreatefromjpeg($originalImage);
-    else if (preg_match('/png/i',$ext))
-        $imageTmp=imagecreatefrompng($originalImage);
-    else if (preg_match('/gif/i',$ext))
-        $imageTmp=imagecreatefromgif($originalImage);
-    else if (preg_match('/bmp/i',$ext))
-        $imageTmp=imagecreatefrombmp($originalImage);
-    else
-        return 0;
+	$target_file = $target_dir . basename($_FILES[$id]["name"]);
+	$uploadOk = 1;
+	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+	$target_file = $target_dir . $id .$imageFileType;
+	$target_file_jpg = $target_dir . $id .".jpg";
+	// Check if image file is a actual image or fake image
+	if(true) {
+	    $check = getimagesize($_FILES[$id]["tmp_name"]);
+	    if($check !== false) {
+	        //echo "File is an image - " . $check["mime"] . ".";
+	        $uploadOk = 1;
+	    } else {
+	        echo "File is not an image.";
+	        $uploadOk = 0;
+	    }
+	}
+	// Check file size
+	if ($_FILES[$id]["size"] > 50*1024*1024) {
+	    echo "Sorry, your file is too large.";
+	    $uploadOk = 0;
+	}
+	// Allow certain file formats
+	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+	&& $imageFileType != "gif" ) {
+	    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+	    $uploadOk = 0;
+	}
+	// Check if $uploadOk is set to 0 by an error
+	if ($uploadOk == 0) {
+	    echo "Sorry, your file was not uploaded.";
+	// if everything is ok, try to upload file
+	} else {
+	    if (move_uploaded_file($_FILES[$id]["tmp_name"], $target_file_jpg)) {
+	        //echo "The file ". basename( $_FILES["idproof"]["name"]). " has been uploaded.";
 
-    // quality is a value from 0 (worst) to 100 (best)
-    imagejpeg($imageTmp, $outputImage, $quality);
-    imagedestroy($imageTmp);
+		//echo "<BR/>FILE TYPE: ".$imageFileType;
+		echo $id . " Photo uploaded";
 
-    return 1;
+
+	    } else {
+	        echo "Sorry, there was an error uploading your file.";
+	    }
+	}
+	return $uploadOk;
 }
+
 ?>
+
+<div id="userDetails">
+  <h1>User Photos</h1>
+  <label>ID Proof</label>
+  <img class="img-id" src="<?php echo $idProof; ?>" atl="ID Proof Not Found" /> 
+  <label>Address Proof</label>
+  <img class="img-addr" src="<?php echo $addressProof; ?>" atl="Address Proof Not Found" /> 
+</div>
+
+
+<style>
+  label {float:left;width:100%;margin-bottom:10px;}
+  
+  #userDetails img {
+    float:left;
+    max-width:100%;
+    max-height:250px;
+  }
+</style>
--- user/user.php
+++ user/user.php
@@ -2,10 +2,88 @@
 include '../lib_security.php';
 
 $uid = isset($_REQUEST['uid']) ? $_REQUEST['uid'] : 1;
+$idProof = "/data/user/{$uid}/idproof.jpg";
+$addressProof = "/data/user/{$uid}/addressproof.jpg";
 ?>
-<h1>User Page</h1>
-<form action="upload.php" method="post" enctype="multipart/form-data">
-<input type="hidden" name="uid" value="<?php echo $uid;?>" />
-<input type="file" name="idproof" />
-<input type="submit" value="upload"/>
-</form>
+<script src='//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js'></script>
+<div>
+  <h3>Upload User Proof Photos</h3>
+  <form action="upload.php" method="post" onsubmit="return validateForm()" enctype="multipart/form-data">
+    <input type="hidden" name="uid" value="<?php echo $uid;?>" />
+    <label>ID Proof</label>
+    <input type="file" name="idproof" />
+    <label>Address Proof</label>
+    <input type="file" name="addressproof" />
+    <input type="submit" class="btn-submit" value="upload"/>
+  </form>
+</div>
+
+<div id="userDetails">
+  <h2>User Photos</h2>
+  <label>ID Proof</label>
+  <img class="img-id" src="<?php echo $idProof; ?>" atl="ID Proof Not Found" /> 
+  <label>Address Proof</label>
+  <img class="img-addr" src="<?php echo $addressProof; ?>" atl="Address Proof Not Found" /> 
+</div>
+
+<div id="progressHld">
+   <div class="loader" style="margin: 0 auto;"></div>
+  Uploading...
+</div>
+
+<script>
+  
+  function validateForm()
+  {
+    $(document).ready(function(){
+      $("#progressHld").show();
+    });
+    return true;
+  }
+
+</script>
+
+<style>
+  form {float:left;width:100%;}
+  label {float:left;width:100%;margin-bottom:10px;}
+  input {
+    float:left;
+    width:100%;
+  }
+  
+  .btn-submit {
+    padding:2%;
+    width:96%;
+    background:#1A237E;
+    color:#FFF;
+    margin:10px 0;
+    outline:0;
+    border:0;
+  }
+  .btn-submit:hover {
+    background:#3949AB;
+  }
+  
+  #progressHld {position:fixed;top:0;left:0;width:100%;height:100%;background:#FFF;display:none;text-align:center;}
+  
+  .loader {
+    border: 16px solid #f3f3f3; /* Light grey */
+    border-top: 16px solid #3498db; /* Blue */
+    border-radius: 50%;
+    width: 120px;
+    height: 120px;
+    animation: spin 2s linear infinite;
+  }
+
+  @keyframes spin {
+      0% { transform: rotate(0deg); }
+      100% { transform: rotate(360deg); }
+  }
+  
+  #userDetails img {
+    float:left;
+    max-width:100%;
+    max-height:150px;
+  }
+
+</style>
